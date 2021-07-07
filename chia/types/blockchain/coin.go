e24f024716c26ea44723331e621e5ff95bde8b6f
Levi Durfee <levi.durfee@gmail.com>
2021-07-07 16:07:36 -0400

    Coins: work on getting coin hash ID

--- chia/types/blockchain/coin.go
+++ chia/types/blockchain/coin.go
@@ -2,20 +2,99 @@
 
 package blockchain
 
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"math/bits"
+)
+
+// 1000000000000 mojo = 1.               XCH =  Chia      = Teramojo = One Trillion Mojos
+// 1000000000    mojo = 0.001            XCH =  MilliChia = Gigamojo
+// 1000000       mojo = 0.000001         XCH =  MicroChia = Megamojo
+// 1000          mojo = 0.000000001      XCH =  NanoChia  = Kilomojo
+// 1             mojo = 0.000000000001   XCH =  PicoChia  = Mojo
+const (
+	Teramojo = 1_000_000_000_000
+	Gigamojo = 1_000_000_000
+	Megamojo = 1_000_000
+	Kilomojo = 1_000
+	Mojo     = 1
+
+	Chia      = 1
+	MilliChia = 0.001
+	MicroChia = 0.000001
+	NanoChia  = 0.000000001
+	PicoChia  = 0.000000000001
+)
+
 // @TODO
 // Coin
 //
 // This structure is used in the body for the reward and fees genesis coins.
 //
-// Fields
+// coin.py
 //
 // 		parent_coin_info: bytes32  # down with this sort of thing.
 // 		puzzle_hash: bytes32
 // 		amount: uint64
 //
-type Coin struct {
-	ParentCoinInfo [32]byte `json:"parent_coin_info"`
-	PuzzleHash     [32]byte `json:"puzzle_hash"`
+// 		def get_hash(self) -> bytes32:
+// 			# This does not use streamable format for hashing, the amount is
+// 			# serialized using CLVM integer format.
 
-	Amount uint64 `json:"amount"`
+// 			# Note that int_to_bytes() will prepend a 0 to integers where the most
+// 			# significant bit is set, to encode it as a positive number. This
+// 			# despite "amount" being unsigned. This way, a CLVM program can generate
+// 			# these hashes easily.
+// 			return std_hash(self.parent_coin_info + self.puzzle_hash + int_to_bytes(self.amount))
+//
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/types/blockchain_format/coin.py
+//
+// hash.py
+//
+// 		def std_hash(b) -> bytes32:
+//     		"""
+//     		The standard hash used in many places.
+//     		"""
+//     		return bytes32(blspy.Util.hash256(bytes(b)))
+//
+// https://github.com/Chia-Network/chia-blockchain/blob/17b061d1d7b76ac01ca8c673331122536354f536/chia/util/hash.py
+//
+// 		std_hash(self.parent_coin_info + self.puzzle_hash + int_to_bytes(self.amount))
+//
+type Coin struct {
+	ParentCoinInfo []byte `json:"parent_coin_info"`
+	PuzzleHash     []byte `json:"puzzle_hash"`
+	Amount         uint64 `json:"amount"`
+}
+
+func (c Coin) ID() []byte {
+	// Convert c.Amount to a []byte
+	size := (bits.Len64(c.Amount) + 8)
+	amount := make([]byte, size*8)
+	binary.BigEndian.PutUint64(amount, c.Amount)
+	fmt.Println(amount)
+
+	hasher := sha256.New()
+	hasher.Write(c.ParentCoinInfo)
+	hasher.Write(c.PuzzleHash)
+	hasher.Write(amount)
+
+	return hasher.Sum(nil)
+}
+
+func (c Coin) StringID() string {
+	return hex.EncodeToString(c.ID())
+}
+
+// ConvertToMojo converts XCH to Mojo
+func ConvertToMojo(amount int) float64 {
+	return float64(amount) / Teramojo
+}
+
+// ConvertToXCH converts Mojo to XCH
+func ConvertToXCH(amount float64) float64 {
+	return amount * float64(Teramojo)
 }
---
+++ chia/types/blockchain/coin_test.go
@@ -0,0 +1,31 @@
+package blockchain
+
+import "testing"
+
+// https://www.chiaexplorer.com/blockchain/coin/0xf7adb0d7ccbe9477cbc1038ab989bbb3db8668a5226640351e8b8fd1b093105d
+func TestCoinStringID(t *testing.T) {
+	// This is the CoinID
+	want := "f7adb0d7ccbe9477cbc1038ab989bbb3db8668a5226640351e8b8fd1b093105d"
+
+	coin := &Coin{
+		ParentCoinInfo: []byte("013e041f0a30d1c3906b3f45f05d58b6458ba7e8ef0d3ae74a5a4b84de7ab7b7"),
+		PuzzleHash:     []byte("3481663ac2c20684d05525027e9207e4a9f7fb66a9539257c4f70e57045d3c9b"),
+		Amount:         Mojo,
+	}
+
+	if coin.StringID() != want {
+		t.Errorf("Want %s, got: %s", want, coin.StringID())
+	}
+}
+
+func TestConvertToMojo(t *testing.T) {
+	if ConvertToMojo(1) != 0.000000000001 {
+		t.Errorf("Want 0.000000000001, got: %f", ConvertToMojo(1))
+	}
+}
+
+func TestConvertToXCH(t *testing.T) {
+	if ConvertToXCH(0.000000000001) != 1 {
+		t.Errorf("Want 1, got: %f", ConvertToXCH(1))
+	}
+}
