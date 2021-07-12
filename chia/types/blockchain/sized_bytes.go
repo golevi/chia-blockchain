24eae1fb20b061178444f374501fc2485e47af1a
Levi Durfee <levi.durfee@gmail.com>
2021-07-12 15:02:35 -0400

    Types: add sized bytes

---
+++ chia/types/blockchain/sized_bytes.go
@@ -0,0 +1,19 @@
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/types/blockchain_format/sized_bytes.py
+
+package blockchain
+
+// bytes4 = make_sized_bytes(4)
+// bytes8 = make_sized_bytes(8)
+// bytes32 = make_sized_bytes(32)
+// bytes48 = make_sized_bytes(48)
+// bytes96 = make_sized_bytes(96)
+// bytes100 = make_sized_bytes(100)
+// bytes480 = make_sized_bytes(480)
+
+type Bytes4 [4]byte
+type Bytes8 [8]byte
+type Bytes32 [32]byte
+type Bytes48 [48]byte
+type Bytes96 [96]byte
+type Bytes100 [100]byte
+type Bytes480 [480]byte
