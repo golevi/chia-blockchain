b08367ae71cfb24896e34e07e68050e3f5b5ac57
Levi Durfee <levi.durfee@gmail.com>
2021-07-12 15:38:30 -0400

    Types: Add VDFProof

--- chia/types/blockchain/vdf.go
+++ chia/types/blockchain/vdf.go
@@ -9,7 +9,19 @@ package blockchain
 // 		output: ClassgroupElement
 //
 type VDFInfo struct {
-	Challenge          []byte `json:"challenge"`
-	NumberOfIterations uint64 `json:"number_of_iterations"`
-	// Output             ClassgroupElement `json:"output"`
+	Challenge          []byte            `json:"challenge"`
+	NumberOfIterations uint64            `json:"number_of_iterations"`
+	Output             ClassgroupElement `json:"output"`
+}
+
+// VDFProof
+//
+// 		witness_type: uint8
+// 		witness: bytes
+// 		normalized_to_identity: bool
+//
+type VDFProof struct {
+	WitnessType          uint8  `json:"witness_type"`
+	Witness              []byte `json:"witness"`
+	NormalizedToIdentity bool   `json:"normalized_to_identity"`
 }
